e7a6091e0f51ee9480e238d37049d6945634d926
TomasMagnin <tomasmagnin@hotmail.com>
2022-09-23 10:01:07 -0300

    v8

--- js/main.js
+++ js/main.js
@@ -37,31 +37,6 @@ cars.push(new Products({brand: "VOLKSWAGEN", model: "GOLF", year: 2022, price: 3
 
 /* ---------- Creamos la funcion y evento para listar los productos---------- */      
 
-
-
-
-/* --------------------------- */
-/* <div class="card" style="width: 18rem;">
-  <img src="..." class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Card title</h5>
-    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-
-const div = document.createElement("div");
-div.textContent = "Esto es un div insertado con JS.";
-
-const app = document.createElement("div"); // <div></div>
-app.id = "app";       // <div id="app"></div>
-app.appendChild(div); // <div id="app"><div>Esto es un div insertado con JS</div></div>
-
- */
-/* ----------------------------------------------------------------------------------------------- */
-
-/* ---------------------------------------------------------------------------------------- */
-
 let contenedor = document.getElementById("ListProdcuts");                       // Traemos el nodo que tiene el atributo product.
 let butt1 = document.getElementById("btn1");                                    // Traemos el nodo que tiene el atributo btn1.
 let butt2 = document.getElementById("btn2");                                    // Traemos el nodo que tiene el atributo btn1.
@@ -70,17 +45,17 @@ let butt2 = document.getElementById("btn2");                                    
 const listado = () => {                                                         // Creamos la funcion listado, para recorrer el array y llamarla mas adelante.
     cars.forEach(item => {                                                      // Recorremos todo el Array compuesto por objetos.
         let div = document.createElement("div");                                // Creamos un div para introducir el listado de productos.
-        div.innerHTML =    `
-                            <div class="card" style="width: 18rem; d-flex flex-direction: row">
-                                <img src="${item.img}" class="card-img-top" alt="">
-                                <div class="card-body">
-                                    <h5 class="card-title"> Marca: ${item.brand} </h5>            
-                                    <p class="card-text"> Modelo: ${item.model} </p>      
-                                    <p class="card-text"> precio $${item.price} </p>
-                                    <a href="#" class="btn btn-primary"> Carrito </a> 
-                                </div>
-                            </div>    
-                            `;                                                  // introducimos en el HTML el listado de productos.
+        div.innerHTML = `
+                        <div class="card" style="width: 18rem; d-flex flex-direction: row">
+                            <img src="${item.img}" class="card-img-top" alt="">
+                            <div class="card-body">
+                                <h5 class="card-title"> Marca: ${item.brand} </h5>            
+                                <p class="card-text"> Modelo: ${item.model} </p>      
+                                <p class="card-text"> precio $${item.price} </p>
+                                <a href="#" class="btn btn-primary"> Carrito </a> 
+                            </div>
+                        </div>    
+                        `;                                                  // introducimos en el HTML el listado de productos.
         contenedor.append(div);                                                 // Insertamos el contenido en la etiqueta div que se va creando anteriormente.
         })
     };     
@@ -89,14 +64,11 @@ butt2.addEventListener("click",() => {                                          
 contenedor.innerHTML  = "";
 }); 
 
-
-
-
-
-/* -------------------------------------------------------------------- */
+/* ---------- Opcion Añadir un Producto ---------- */   
 
 let formulario1 = document.getElementById("form");                              // Traemos el nodo que tiene el atributgo form.
 let contenedor2 = document.getElementById("products2");                         // Traemos el nodo que tiene el atributo product.
+
 
 formulario1.addEventListener("submit", (e) => {                                 // Utilizando el evento "submit" creamos una funcion para extraer los datos tal cual fueron introducidos en la pagina.
     e.preventDefault();
@@ -109,11 +81,9 @@ formulario1.addEventListener("submit", (e) => {                                 
     let input5 = formVar[13].value;
     cars.push(new Products({brand: `${input1}`, model: `${input2}`, year: `${input3}`, price: `${input4}`, stock: `${input5}`, sold: false}));
     
-   
-   
-    cars.forEach(item => {                                                      // Recorremos todo el Array compuesto por objetos.
-        let div = document.createElement("div");                                // Creamos un div para introducir el listado de productos.
-        div.innerHTML =    `
+    cars.forEach(item  => {                                                      // Recorremos todo el Array compuesto por objetos.
+        let div1 = document.createElement("div");                                // Creamos un div para introducir el listado de productos.
+        div1.innerHTML =    `
                             <div class="card" style="width: 18rem; d-flex flex-direction: row">
                                 <img src="${item.img}" class="card-img-top" alt="">
                                 <div class="card-body">
@@ -124,51 +94,10 @@ formulario1.addEventListener("submit", (e) => {                                 
                                 </div>
                             </div>    
                             `;                                                  // introducimos en el HTML el listado de productos.
-        contenedor.append(div);                                                 // Insertamos el contenido en la etiqueta div que se va creando anteriormente.
+        contenedor2.append(div1);                                                 // Insertamos el contenido en la etiqueta div que se va creando anteriormente.
         })
-    
-   
-    /* cars.forEach(item2 => {                                                     // Recorremos todo el Array con el nuevo producto.
-        let div1 = document.createElement("div" && "p");                        // Creamos la etiqueta div y p.
-        div1.innerHTML = item2.showCars();                                      // introducimos el listado en el HTML. 
-        contenedor2.append(div1);                                               // Agregamos el contenido a la etiqueta div > p.
-    }) */
-});
-
-
-
-
-
-
-
-
-
-/* ---------- Opcion Añadir un Producto ---------- */   
+    });
                
-/* let formulario1 = document.getElementById("form");                              // Traemos el nodo que tiene el atributgo form.
-let contenedor2 = document.getElementById("products2");                         // Traemos el nodo que tiene el atributo product.
-
-formulario1.addEventListener("submit", (e) => {                                 // Utilizando el evento "submit" creamos una funcion para extraer los datos tal cual fueron introducidos en la pagina.
-    e.preventDefault();
-    let formVar = e.target.children;
-    let input1 = formVar[1].value.toUpperCase();                                // Asginamos a la variable  input1 el el valor(value) de la etiqueta(tag) en la posicion 1 del nodo padre "form", nos devuelve el texto tal cual lo escribimos.   
-    //let input1 = document.getElementById("brandField").value;                 // Otra forma de traer el contenido del imput pero no se si es correcto.
-    let input2 = formVar[4].value.toUpperCase();                                // Asginamos a la variable  input2 el el valor(value) de la etiqueta(tag) en la posicion 4 del nodo padre "form", nos devuelve el texto tal cual lo escribimos.
-    let input3 = formVar[7].value;                                              // Asginamos a la variable  input3 el el valor(value) de la etiqueta(tag) en la posicion 7 del nodo padre "form", nos devuelve el texto tal cual lo escribimos.                   
-    let input4 = formVar[10].value;                                             // Asginamos a la variable  input3 el el valor(value) de la etiqueta(tag) en la posicion 10 del nodo padre "form", nos devuelve el texto tal cual lo escribimos.
-    let input5 = formVar[13].value;
-    cars.push(new Products({brand: `${input1}`, model: `${input2}`, year: `${input3}`, price: `${input4}`, stock: `${input5}`, sold: false}));
-    
-    cars.forEach(item2 => {                                                     // Recorremos todo el Array con el nuevo producto.
-        let div1 = document.createElement("div" && "p");                        // Creamos la etiqueta div y p.
-        div1.innerHTML = item2.showCars();                                      // introducimos el listado en el HTML. 
-        contenedor2.append(div1);                                               // Agregamos el contenido a la etiqueta div > p.
-    })
-}); */
-      
-
-
-
 /* ---------- Opcion para buscar un Producto ---------- */      
 
 let formulario2 = document.getElementById("form2");                             // Traemos el nodo que tiene el atributo form2.
@@ -197,18 +126,11 @@ formulario2.addEventListener("submit", (e) => {                                 
         });
 });            
  
-
-
-
-
 /* ---------- Sesion de Usuario ---------- */
 
-
-
-let usx = getElementById("formNav");
+let usx = document.getElementById("formNav");
 let usuario;                                                                    // Creamos la variable usuario
 let usuarioStorage = sessionStorage.getItem("usuario");                         // Traemos del sessionStorage el contenido de la variable
-
 
 if(usuarioStorage){                                                             // Si la variable tiene contenido.
     let mensaje = `Bienvenido ${usuarioStorage}`;                               // Entoces sale el cartel de saludo
